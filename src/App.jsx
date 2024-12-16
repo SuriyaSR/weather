de4c084642d7de69cbc892b04e007dd3abd6f914
SuriyaSR <suriyasun19@gmail.com>
2024-12-16 22:24:32 +0530

    Created UI elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,74 @@
 import { useState } from 'react'
 import './App.css'
 
+/*Images*/
+import clearIcon from './assets/clear.png';
+import cloudIcon from './assets/cloud.png';
+import drizzleIcon from './assets/drizzle.png';
+import humidityIcon from './assets/humidity.png';
+import rainIcon from './assets/rain.png';
+import searchIcon from './assets/search.png';
+import snowIcon from './assets/snow.png';
+import windIcon from './assets/wind.png';
+
+const WeatherDetails = ({icon, temp, city, country, lat, long, humidity, wind}) =>{
+  return (<>
+  <div className='image'>
+    <img src={icon} alt="Image" />
+  </div>
+  <div className="temp">{temp}°C</div>
+  <div className="location">{city}</div>
+  <div className="country">{country}</div>
+  <div className="cord">
+    <div>
+      <span className="lat">latitude</span>
+      <span>{lat}</span>
+    </div>
+    <div>
+      <span className="long">longitude</span>
+      <span>{long}</span>
+    </div>
+  </div>
+  <div className="data-container">
+    <div className="element">
+      <img src={humidityIcon} alt="humidity" className="icon" />
+      <div className="data">
+        <div className="humidity-percent">{humidity}%</div>
+        <div className="text">Humidity</div>
+      </div>
+    </div>
+    <div className="element">
+      <img src={windIcon} alt="wind" className="icon" />
+      <div className="data">
+        <div className="wind-percent">{wind} km/h</div>
+        <div className="text">Wind Speed</div>
+      </div>
+    </div>
+  </div>
+  </>);
+}
 function App() {  
+  const [icon, setIcon] = useState(snowIcon);
+  const [temp, setTemp] = useState(0);
+  const [city, setCity] = useState("Chennai");
+  const [country, setCountry] = useState("IN");
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [wind, setWind] = useState(0);
   return (
     <>
-      <div>
-        
+      <div className='container'>
+        <div className='input-container'>
+          <input type="text" className="cityInput" placeholder="Search City" />
+          <div className="search-icon">
+            <img src={searchIcon} alt="Search" />
+          </div>
+        </div>
+        <WeatherDetails icon={icon} temp={temp} city={city} country={country} lat={lat} long={long} humidity={humidity} wind={wind}/>
+      <p className="copyright">
+        Designed By <span>Suriya J</span>
+      </p>
       </div>
     </>
   )
